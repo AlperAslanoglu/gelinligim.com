6dbfe2b900f2b506ddee40f169c601fa6852628c
gelinligim server <aslanoglu@gmail.com>
2026-09-09 00:04:47 +0300

    Sunucudan içe aktarma: uploads (kalan dosyalar)

---
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -0,0 +1,7 @@
+<?php
+// datastore=auditqueue;
+// created_on=1608803728;
+// updated_on=1608803728;
+exit(0);
+?>
+1608803728_5934:"Debug: esra, 78.188.16.35; Sucuri plugin has been uninstalled"
---
+++ wp-content/uploads/sucuri/sucuri-ignorescanning.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=ignorescanning;
+// created_on=1608803728;
+// updated_on=1608803728;
+exit(0);
+?>
